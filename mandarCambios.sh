c5a0ff939f358149a7c98af44fc624f2a4fe1673
valentinCorrea25 <correavalentin89@gmail.com>
2024-07-09 18:11:03 -0300

    update by valentinCorrea25

---
+++ mandarCambios.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+usuario="valentinCorrea25"
+contrasenia="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"
+repo="ClubSocialVillaSerranaPage.git"
+
+# Configurar las credenciales para la URL del repositorio remoto
+url="https://$usuario:$contrasenia@github.com/$usuario/$repo"
+
+#cd HotecheUruguayMarchShopsUpdate
+
+# Agregar todos los cambios al área de staging
+git add .
+
+# Realizar un commit con un mensaje predefinido
+git commit -m "update by $usuario"
+
+# Hacer push a la rama actual en el repositorio remoto (por defecto, 'origin')
+git push "$url"
+
