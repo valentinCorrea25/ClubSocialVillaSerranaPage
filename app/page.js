fef86184e4d8257e1606e4c9ef2c9debd05f4b14
valentinCorrea25 <correavalentin89@gmail.com>
2024-07-18 15:25:29 -0300

    ant

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
-import Test from "@/components/home/Test";
-import { DatePicker } from 'antd';
+import Footer from "@/components/home/Footer";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center text-5xl p-20">
-      <DatePicker />
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-1 flex justify-center items-center text-5xl p-20">
+
+        Work in Progress...
+        
+      </main>
+      <Footer />
     </div>
   );
 }
