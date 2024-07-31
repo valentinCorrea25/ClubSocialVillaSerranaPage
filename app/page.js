dfd84be93346a2eff18d9dd90c6b7143683d0ed3
tomascontilde <tomazcontilde@gmail.com>
2024-07-31 12:55:55 -0300

    ´prueba´

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center text-5xl p-20">
+    <div className="flex justify-center items-center text-5xl p-20 bg-red-800">
       Work in Progress...
     </div>
   );
