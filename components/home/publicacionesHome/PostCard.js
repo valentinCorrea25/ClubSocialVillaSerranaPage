8139ca8fb703a8f2aae8e0e0663c96751b414d80
tomascontilde <tomazcontilde@gmail.com>
2024-08-08 15:53:07 -0300

    increasing changes

--- components/home/publicacionesHome/PostCard.js
+++ components/home/publicacionesHome/PostCard.js
@@ -7,7 +7,7 @@ const PostCard = ({ post }) => {
     <div className="bg-white p-6 rounded-xl shadow-lg ">
       <img src={post.image} alt={post.title} className=" mx-auto" />
       <h2 className="text-xl font-extrabold mb-2">{post.title}</h2>
-      <p className="text-gray-700 text-justify">  <CiCalendar /> {post.description.substring(0, 100)}...
+      <p className="text-gray-700 text-justify">  <CiCalendar className="inline "/> {post.description.substring(0, 100)}...
         <button className="text-[--verde] ml-2 hover:text-[--verder-super-oscuro]" >Ver mas</button>
       </p>
 
