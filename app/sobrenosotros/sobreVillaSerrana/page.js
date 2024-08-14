7fe4c957a781dd8fc04a3b862d7b440f5edb890a
tomascontilde <tomazcontilde@gmail.com>
2024-08-14 12:44:09 -0300

    sobre nosotros

---
+++ app/sobrenosotros/sobreVillaSerrana/page.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Sobrevillaserrana} from '@/components/SobreNosotros/SobreCLubyVillaserana'
+
+export default function SobreNosotros() {
+  return (
+      <Sobrevillaserrana/>
+  )
+}
