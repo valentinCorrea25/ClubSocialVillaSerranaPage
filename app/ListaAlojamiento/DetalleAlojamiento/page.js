e0938f6ebe6fc3dadb9ffd1d3d1234df66553c3c
valentinCorrea25 <correavalentin89@gmail.com>
2024-08-25 19:09:07 -0300

    fix Detalle Alojamientos

--- app/ListaAlojamiento/DetalleAlojamiento/page.js
+++ app/ListaAlojamiento/DetalleAlojamiento/page.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { alojamientos as allAlojamientos } from '@/test/data'; 
+import { Suspense } from 'react';
 import Banner from '@/components/ListaAlojamiento/DetalleAlojamiento/Banner';
 import Informacion from '@/components/ListaAlojamiento/DetalleAlojamiento/Informacion'; 
 import Carousel from '@/components/ListaAlojamiento/DetalleAlojamiento/Carousel';
@@ -31,25 +32,27 @@ const DetalleAlojamientos = () => {
   }
 
   return (
-    <div className="container flex flex-col min-h-screen bg-[#F9F6EE] px-12 sm:px-16 lg:px-24 py-32"> 
-      <div className="mb-14"><Banner title="Alojamientos" subtitle="Descubre nuestras opciones" 
-      backgroundImage="/images/alojamiento.jpg" /></div>
+      <Suspense>
+        <div className="container flex flex-col min-h-screen bg-[#F9F6EE] px-12 sm:px-16 lg:px-24 py-32"> 
+          <div className="mb-14"><Banner title="Alojamientos" subtitle="Descubre nuestras opciones" 
+          backgroundImage="/images/alojamiento.jpg" /></div>
 
-    <main className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-10"> 
-      <div className="lg:col-span-2 flex flex-col gap-8"> 
-        <Informacion alojamiento={alojamientoSeleccionado} />
-        <Carousel alojamiento={alojamientoSeleccionado} />
-        <Descripcion alojamiento={alojamientoSeleccionado} />
-        <Caracteristicas caracteristicas={alojamientoSeleccionado.caracteristicas} />
-        </div>
+        <main className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-10"> 
+          <div className="lg:col-span-2 flex flex-col gap-8"> 
+            <Informacion alojamiento={alojamientoSeleccionado} />
+            <Carousel alojamiento={alojamientoSeleccionado} />
+            <Descripcion alojamiento={alojamientoSeleccionado} />
+            <Caracteristicas caracteristicas={alojamientoSeleccionado.caracteristicas} />
+          </div>
 
-    <div className="lg:col-span-1 flex flex-col gap-8"><div className="flex-1 flex flex-col gap-8"> 
-        <div className="w-full"><Contacto contacto={alojamientoSeleccionado.contacto} /></div>
-        <UbicacionMap ubicacion={alojamientoSeleccionado.location} /></div>
-        <div className="flex-1"><PublicacionesSimilares onSelect={setAlojamientoSeleccionado} /></div>
+          <div className="lg:col-span-1 flex flex-col gap-8"><div className="flex-1 flex flex-col gap-8"> 
+            <div className="w-full"><Contacto contacto={alojamientoSeleccionado.contacto} /></div>
+            <UbicacionMap ubicacion={alojamientoSeleccionado.location} /></div>
+            <div className="flex-1"><PublicacionesSimilares onSelect={setAlojamientoSeleccionado} /></div>
+          </div>
+        </main>
       </div>
-    </main>
-  </div>
+    </Suspense>
   );
 };
 
