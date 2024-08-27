5e4020889314d381ae852f9b596eeb55b2487166
valentinCorrea25 <correavalentin89@gmail.com>
2024-08-26 22:39:00 -0300

    video sapeee

--- app/page.js
+++ app/page.js
@@ -4,12 +4,15 @@ import ListaCatHome from "@/components/home/ListaCatHome/ListaCatHome";
 import { ListaDeUltimasPublicaciones } from "@/components/home/publicacionesHome/PostList";
 import Mapa from "@/components/home/MostrarMapa/Mapa";
 import SobreNosotros from "@/components/home/SobreNosotros/SobreNosotros";
+import MainVideo from "@/components/home/MainVideo";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-[#F9F6EE]">
+      <MainVideo/>
       <main className="flex-1 flex justify-center items-center text-5xl p-20">
-        {/*<ListaCatHome />*/}
+        <ListaCatHome />
       </main>
       <ListaDeUltimasPublicaciones />
       <Mapa />
---
+++ components/home/MainVideo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import videoBG from "@/public/Villa.mp4";
+export default function MainVideo() {
+  return (
+    <div className="w-full h-[100vh] relative">
+      <div className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,.4)]"></div>
+      <video
+        src={videoBG}
+        autoPlay
+        muted
+        loop
+        className="w-full h-full object-cover"
+      />
+      <div className="absolute w-full h-full top-0 flex flex-col justify-center items-center text-white p-10">
+        <h1 className="text-center text-5xl">Bienvenido a Villa Serrana</h1>
+        <p className="max-w-[1000px] text-center">
+          Villa Serrana es un oasis natural en Uruguay, con paisajes hermosos,
+          arquitectura única, y una tranquilidad perfecta para desconectar y
+          recargar energías. Un destino ideal para amantes de la naturaleza y la
+          paz.
+        </p>
+      </div>
+    </div>
+  );
+}
