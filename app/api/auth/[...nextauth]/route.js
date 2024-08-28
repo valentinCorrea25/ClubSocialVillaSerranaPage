6a79020fc1a3b222b659c685537728d541c2dd48
valentinCorrea25 <correavalentin89@gmail.com>
2024-08-28 17:49:28 -0300

    test login

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,48 +1,52 @@
 import prisma from "@/libs/prisma";
 import NextAuth from "next-auth";
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 import CredentialsProviders from "next-auth/providers/credentials";
 
 export const authOptions = {
-    providers:[
-        CredentialsProviders({
-            name: 'Credentials',
-            credentials:{
-                userName: {label: 'Nombre', type:'text', placeholder: 'Nombre de Usuario' },
-                password: {label: 'Contrasenia', type: 'password', placeholder: 'Contrasenia'}
-            },
-            async authorize(credentials, req){
-                try{
-                    const userFound = await prisma.usuario.findFirst(
-                        {where: {
-                            nombre: credentials.userName,
-                    }, });
-    
-                    if(!userFound){
-                        throw new Error('Usuario no encontrado')
-                    }
-    
-                    const itHasCorrectPassword = await bcrypt.compare(credentials.password, userFound.hashPass);
-                    if(!itHasCorrectPassword){
-                        console.log(itHasCorrectPassword);
-                        throw new Error('Clave incorrecta')
-                    }
-    
-                    return {id: userFound.id,
-                            name: userFound.nombre,
-                    };
-                }
-                catch(error){
-                    console.log(error);
-                    return {message: 'Ha ocurrido un error'};
-                }
-            }
-        })
-    ],
-    pages:{
-        signIn: '/admin'
-    }
-}
+  providers: [
+    CredentialsProviders({
+      name: "Credentials",
+      credentials: {
+        userName: {
+          label: "Nombre",
+          type: "text",
+          placeholder: "Nombre de Usuario",
+        },
+        password: {
+          label: "Contrasenia",
+          type: "password",
+          placeholder: "Contrasenia",
+        },
+      },
+      async authorize(credentials, req) {
+        const userFound = await prisma.usuario.findFirst({
+          where: {
+            nombre: credentials.userName,
+          },
+        });
+
+        if (!userFound) {
+          throw new Error("Usuario no encontrado");
+        }
+
+        const itHasCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          userFound.hashPass
+        );
+        if (!itHasCorrectPassword) {
+          console.log(itHasCorrectPassword);
+          throw new Error("Clave incorrecta");
+        }
+
+        return { id: userFound.id, name: userFound.nombre };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/admin",
+  },
+};
 
 const handler = NextAuth(authOptions);
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
