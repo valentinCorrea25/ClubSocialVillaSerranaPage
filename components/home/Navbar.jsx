c047f2a800d18b1985b24b84e24bb0d773f2bc4b
tomascontilde <tomazcontilde@gmail.com>
2024-09-12 11:44:20 -0300

    que encuentras si hay algo que no le gusta avisen y cambio. Tambien ya empiezo con el coso ese del homenashe

--- components/home/Navbar.jsx
+++ components/home/Navbar.jsx
@@ -1,17 +1,68 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import logo from '@/public/images/logo.png';
 import { useRouter } from 'next/navigation';
 import { IoMenuSharp } from "react-icons/io5";
+import { DownOutlined } from '@ant-design/icons';
+import { Dropdown, Space } from 'antd';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false); // Estado para manejar la visibilidad del menú
   const router = useRouter();
+  const [isHovered, setIsHovered] = useState(false); // Estado para hover en el Dropdown o su contenido
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+  const [dropdownTrigger, setDropdownTrigger] = useState(['hover']);
+  useEffect(() => {
+    const updateTrigger = () => {
+      if (window.innerWidth <= 768) {
+        setDropdownTrigger(['click']);
+      } else {
+        setDropdownTrigger(['hover']); 
+      }
+    };
+    updateTrigger();
+    window.addEventListener("resize", updateTrigger);
+    return () => window.removeEventListener("resize", updateTrigger);
+  }, []);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+   const items = [
+      {
+        label: (
+          <a className="relative block w-full min-w-full py-2 px-4 hover:text-gray-900 hover:bg-green-300 hover:rounded-md hover:border-r-4 hover:border-[--verde] transition-all m-0"
+          onClick={() => router.push('/ListaRestaurantes')} target="_blank" >
+            Restaurantes
+          </a>
+        ),
+      },
+      {
+        label: (
+          <a className="relative block w-full min-w-full py-2 px-4 hover:text-gray-900 hover:bg-green-300 hover:rounded-md hover:border-r-4 hover:border-[--verde] transition-all m-0"
+          onClick={() => router.push('/ListaAlojamiento')} target="_blank">
+            Alojamientos
+          </a>
+        ),
+      },
+      { 
+        label: (
+          <a className="relative block w-full min-w-full py-2 px-4  hover:text-gray-900 hover:bg-green-300 hover:rounded-md hover:border-r-4 hover:border-[--verde] transition-all m-0"
+          onClick={() => router.push('/ListaActividades')} target="_blank" >
+            Actividades
+          </a>
+        ),
+      },{
+        label: (
+          <a className="relative block w-full min-w-full py-2 px-4  hover:text-gray-900 hover:bg-green-300 hover:rounded-md hover:border-r-4 hover:border-[--verde] transition-all m-0"
+          onClick={() => router.push('/ListaEventosNoticias')} target="_blank">
+            Eventos e Noticias
+          </a>
+        ),
+      },
+    ]
 
   return (
     <nav className="bg-[--blanco] border-b-4 border-[--verder-super-oscuro] montserrat z-[5000]">
@@ -36,7 +87,43 @@ export default function Navbar() {
               Home
             </li>
             <li>
-              <a href="/QueEncuentras" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent hover:text-[--verde] transition-all lg:border-0">Que encuentras</a>
+              {/* <a href="/QueEncuentras" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent hover:text-[--verde] transition-all lg:border-0">Que encuentras</a> */}
+                <Dropdown
+                  overlay={
+                    <div 
+                      className="bg-white border border-gray-200 rounded-lg"
+                      onMouseEnter={handleMouseEnter}  
+                      onMouseLeave={handleMouseLeave} 
+                    >
+                      {items.map((item, index) => (
+                        <div key={index} className="">
+                          {item.label}
+                        </div>
+                      ))}
+                    </div>
+                  }
+                  trigger={dropdownTrigger} // Cambia entre 'click' y 'hover'
+                  onVisibleChange={(visible) => setIsHovered(visible)} // También cambia el estado cuando el menú está visible
+                >
+                  <div
+                    className="block"
+                    onMouseEnter={handleMouseEnter}  // Activa hover cuando el mouse entra al Dropdown
+                    onMouseLeave={handleMouseLeave}  // Desactiva hover cuando el mouse sale del Dropdown
+                  >
+                    <a
+                      className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent hover:text-[--verde] transition-all lg:border-0"
+                    >
+                      <Space className="flex items-center">
+                        <span className={`${isHovered ? 'text-[--verde]' : ''} transition-all`}>
+                          Que encuentras
+                        </span>
+                        <DownOutlined
+                          className={`transform transition-transform duration-300  ${isHovered ? 'rotate-0 text-[--verde]' : 'rotate-90 '}`}
+                        />
+                      </Space>
+                    </a>
+                  </div>
+                </Dropdown>
             </li>
             <li>
               <a href="/sobrenosotros" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent hover:text-[--verde] transition-all lg:border-0">Sobre Nosotros</a>
