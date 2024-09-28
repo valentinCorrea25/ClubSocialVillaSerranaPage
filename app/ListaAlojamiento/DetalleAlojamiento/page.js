a1de5dd8010b7fe7e9fda8d8c03496fc380967e3
valentinCorrea25 <correavalentin89@gmail.com>
2024-09-28 15:24:31 -0300

    fix

--- app/ListaAlojamiento/DetalleAlojamiento/page.js
+++ app/ListaAlojamiento/DetalleAlojamiento/page.js
@@ -1,103 +1,76 @@
-'use client'
-import React, { useState, useEffect, useContext, useMemo } from "react";
-import { useRouter } from "next/navigation";
-import { Spin } from "antd";
-import { ClientContext } from "@/context/clientContext";
-import Banner from "@/components/ListaAlojamiento/DetalleAlojamiento/Banner";
-import Informacion from "@/components/ListaAlojamiento/DetalleAlojamiento/Informacion";
-import Carousel from "@/components/ListaAlojamiento/DetalleAlojamiento/Carousel";
-import Descripcion from "@/components/ListaAlojamiento/DetalleAlojamiento/Descripcion";
-import Caracteristicas from "@/components/ListaAlojamiento/DetalleAlojamiento/Caracteristicas";
-import UbicacionMap from "@/components/ListaAlojamiento/DetalleAlojamiento/UbicacionMap";
-import PublicacionesSimilares from "@/components/ListaAlojamiento/DetalleAlojamiento/PublicacionesSimilares";
+'use client';
+import React, { useState, useEffect, useContext } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { alojamientos as allAlojamientos } from '@/test/data'; 
+import { Suspense } from 'react';
+import Banner from '@/components/ListaAlojamiento/DetalleAlojamiento/Banner';
+import Informacion from '@/components/ListaAlojamiento/DetalleAlojamiento/Informacion'; 
+import Carousel from '@/components/ListaAlojamiento/DetalleAlojamiento/Carousel';
+import Descripcion from '@/components/ListaAlojamiento/DetalleAlojamiento/Descripcion';
+import Caracteristicas from '@/components/ListaAlojamiento/DetalleAlojamiento/Caracteristicas';
+import Contacto from '@/components/ListaRestaurantes/DetalleRestaurante/Contacto';
+import UbicacionMap from '@/components/ListaAlojamiento/DetalleAlojamiento/UbicacionMap'; 
+import PublicacionesSimilares from '@/components/ListaAlojamiento/DetalleAlojamiento/PublicacionesSimilares';
+import { ClientContext } from '@/context/clientContext';
+import { Spin } from 'antd';
 
-const useAccommodationData = (id) => {
-  const { buscarAlquiler } = useContext(ClientContext);
-  const [data, setData] = useState({ alojamiento: null, similares: null });
+const DetalleAlojamientos = () => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id'); 
+  const {buscarAlquiler} = useContext(ClientContext);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [alojamientoSeleccionado, setAlojamientoSeleccionado] = useState();
+  const [alojamientoSimilares, setAlquileresSimilares] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
       if (id) {
-        try {
-          setIsLoading(true);
-          const resultado = await buscarAlquiler(id);
-          if (resultado) {
-            setData({
-              alojamiento: resultado.publicacion,
-              similares: resultado.publicacionesRelacionadas,
-            });
-          } else {
-            setError("Alojamiento no encontrado");
-          }
-        } catch (err) {
-          setError(err.message || "Error al cargar los datos");
-        } finally {
-          setIsLoading(false);
-        }
+        setIsLoading(true);
+        const resultado = await buscarAlquiler(id);
+        console.log(resultado);
+        
+        setAlojamientoSeleccionado(resultado.publicacion);
+        setAlquileresSimilares(resultado.publicacionesRelacionadas)
+        setIsLoading(false);
       }
     };
     fetchData();
-  }, [id, buscarAlquiler]);
+  }, [id]);
 
-  return { data, isLoading, error };
-};
-
-const DetalleAlojamientos = () => {
-  const router = useRouter();
-  const { id } = router.query || {};
-  const { data, isLoading, error } = useAccommodationData(id);
-
-  const memoizedContent = useMemo(() => {
-    if (isLoading) {
-      return (
-        <div className="flex justify-center items-center min-h-screen">
-          <Spin size="large" aria-label="Cargando detalles del alojamiento" />
-        </div>
-      );
-    }
-
-    if (error) {
-      return <div role="alert" className="text-center text-red-500">{error}</div>;
-    }
-
-    if (!data.alojamiento) {
-      return <div role="alert" className="text-center">Alojamiento no encontrado</div>;
-    }
-
+  if (isLoading) {
     return (
-      <main className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-10">
-        <div className="lg:col-span-2 flex flex-col gap-8">
-          <Informacion alojamiento={data.alojamiento} />
-          <Carousel alojamiento={data.alojamiento} />
-          <Descripcion alojamiento={data.alojamiento} />
-          <Caracteristicas alojamiento={data.alojamiento} />
-        </div>
-
-        <div className="lg:col-span-1 flex flex-col gap-8">
-          <div className="flex-1 flex flex-col gap-8">
-            <UbicacionMap ubicacion={data.alojamiento.ubicacion} />
-          </div>
-          <div className="flex-1">
-            <PublicacionesSimilares similares={data.similares} />
-          </div>
-        </div>
-      </main>
+      <div className="flex justify-center items-center min-h-screen">
+        <Spin size="large" />
+      </div>
     );
-  }, [data, isLoading, error]);
+  }
+
+  if (!alojamientoSeleccionado) {
+    return <div>Alojamiento no encontrado</div>;
+  }
 
   return (
-    <div className="container flex flex-col max-w-screen-xl bg-[#F9F6EE] px-12 sm:px-16 lg:px-24 py-24 m-auto h-full">
-      <div className="mb-14">
-        <Banner
-          title="Alojamientos"
-          subtitle="Descubre nuestras opciones"
-          backgroundImage="/images/alojamiento.jpg"
-        />
+      <Suspense>
+        <div className="container flex flex-col max-w-screen-xl bg-[#F9F6EE] px-12 sm:px-16 lg:px-24 py-24 m-auto h-full "> 
+          <div className="mb-14"><Banner title="Alojamientos" subtitle="Descubre nuestras opciones" 
+          backgroundImage="/images/alojamiento.jpg" /></div>
+
+        <main className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-10"> 
+          <div className="lg:col-span-2 flex flex-col gap-8"> 
+            <Informacion alojamiento={alojamientoSeleccionado} />
+            <Carousel alojamiento={alojamientoSeleccionado} />
+            <Descripcion alojamiento={alojamientoSeleccionado} />
+            <Caracteristicas alojamiento={alojamientoSeleccionado} />
+          </div>
+
+          <div className="lg:col-span-1 flex flex-col gap-8"><div className="flex-1 flex flex-col gap-8"> 
+            {/* <div className="w-full"><Contacto alquiler={alojamientoSeleccionado} /></div> */}
+            <UbicacionMap ubicacion={alojamientoSeleccionado.ubicacion} /></div>
+            <div className="flex-1"><PublicacionesSimilares similares={alojamientoSimilares} /></div>
+          </div>
+        </main>
       </div>
-      {memoizedContent}
-    </div>
+    </Suspense>
   );
 };
 
--- app/ListaRestaurantes/DetalleRestaurante/page.js
+++ app/ListaRestaurantes/DetalleRestaurante/page.js
@@ -1,102 +1,95 @@
-'use client'
-import React, { useState, useEffect, useContext, useMemo } from "react";
-import { useRouter } from "next/navigation";
-import { Spin } from "antd";
-import { ClientContext } from "@/context/clientContext";
-import Banner from "@/components/ListaRestaurantes/DetalleRestaurante/Banner";
-import Informacion from "@/components/ListaRestaurantes/DetalleRestaurante/Informacion";
-import Carousel from "@/components/ListaRestaurantes/DetalleRestaurante/Carousel";
-import Caracteristicas from "@/components/ListaRestaurantes/DetalleRestaurante/Caracteristicas";
-import Contacto from "@/components/ListaRestaurantes/DetalleRestaurante/Contacto";
-import UbicacionMap from "@/components/ListaRestaurantes/DetalleRestaurante/UbicacionMap";
-import PublicacionesSimilares from "@/components/ListaRestaurantes/DetalleRestaurante/PublicacionesSimilares";
+'use client';
 
-const useRestaurantData = (id) => {
+import React, { useState, useEffect, useContext } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { Spin } from 'antd'; // Importa el spinner de antd
+import { ClientContext } from '@/context/clientContext';
+import { Suspense } from 'react';
+import Banner from '@/components/ListaRestaurantes/DetalleRestaurante/Banner';
+import Informacion from '@/components/ListaRestaurantes/DetalleRestaurante/Informacion';
+import Carousel from '@/components/ListaRestaurantes/DetalleRestaurante/Carousel';
+import Descripcion from '@/components/ListaRestaurantes/DetalleRestaurante/Descripcion';
+import Caracteristicas from '@/components/ListaRestaurantes/DetalleRestaurante/Caracteristicas';
+import Contacto from '@/components/ListaRestaurantes/DetalleRestaurante/Contacto';
+import UbicacionMap from '@/components/ListaRestaurantes/DetalleRestaurante/UbicacionMap';
+import PublicacionesSimilares from '@/components/ListaRestaurantes/DetalleRestaurante/PublicacionesSimilares';
+
+const DetalleRestaurantes = () => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
   const { buscarRestaurant } = useContext(ClientContext);
-  const [data, setData] = useState({ restaurante: null, similares: null });
+  const [restauranteSeleccionado, setRestauranteSeleccionado] = useState(null);
+  const [ restaurantesSimilares, setRestaurantesSimilares ] = useState(null);  
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchRestaurant = async () => {
       if (id) {
-        try {
-          setIsLoading(true);
-          const resultado = await buscarRestaurant(id);
-          setData({
-            restaurante: resultado.publicacion,
-            similares: resultado.publicacionesRelacionadas,
-          });
-        } catch (err) {
-          setError(err.message);
-        } finally {
-          setIsLoading(false);
-        }
+        setIsLoading(true);
+        const resultado = await buscarRestaurant(id);
+        setRestauranteSeleccionado(resultado.publicacion);
+        setRestaurantesSimilares(resultado.publicacionesRelacionadas)
+        setIsLoading(false);
       }
     };
     fetchRestaurant();
-  }, [id, buscarRestaurant]);
-
-  return { data, isLoading, error };
-};
-
-const DetalleRestaurantes = () => {
-  const router = useRouter();
-  const { id } = router.query || {};
-  const { data, isLoading, error } = useRestaurantData(id);
-
-  const memoizedContent = useMemo(() => {
-    if (isLoading) {
-      return (
-        <div className="flex justify-center items-center min-h-screen">
-          <Spin size="large" aria-label="Cargando detalles del restaurante" />
-        </div>
-      );
-    }
-
-    if (error) {
-      return <div role="alert">Error: {error}</div>;
-    }
-
-    if (!data.restaurante) {
-      return <div role="alert">Restaurante no encontrado</div>;
-    }
-
+  }, [id]);
+  
+  if (isLoading) {
     return (
-      <main className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
-        <div className="lg:col-span-2 flex flex-col gap-6">
-          <Informacion restaurante={data.restaurante} />
-          <Carousel restaurante={data.restaurante} />
-          <Caracteristicas restaurante={data.restaurante} />
-        </div>
-        <div className="lg:col-span-1 flex flex-col h-full gap-6">
-          <div className="flex flex-col flex-1 gap-6">
-            <div className="flex-1">
-              <Contacto restaurante={data.restaurante} />
-            </div>
-            <div className="flex-1">
-              <UbicacionMap ubicacion={data.restaurante.coordenadas} />
-            </div>
-          </div>
-          <div className="mt-auto">
-            <PublicacionesSimilares restaurantes={data.similares} />
-          </div>
-        </div>
-      </main>
+      <div className="flex justify-center items-center min-h-screen">
+        <Spin size="large" /> {/* Spinner */}
+      </div>
     );
-  }, [data, isLoading, error]);
+  }
+
+  if (!restauranteSeleccionado) {
+    return <div>Restaurante no encontrado</div>;
+  }
+  
+  
 
   return (
-    <div className="flex flex-col min-h-screen max-w-screen-xl m-auto bg-[#F9F6EE] px-8 sm:px-12 lg:px-20 py-24">
-      <div className="mb-10">
-        <Banner
-          title="Restaurantes"
-          subtitle="Descubre nuestras opciones"
-          backgroundImage="/images/restaurantes.jpg"
-        />
+    <Suspense>
+      <div className="flex flex-col min-h-screen max-w-screen-xl m-auto bg-[#F9F6EE] px-8 sm:px-12 lg:px-20 py-24">
+        <div className="mb-10">
+          <Banner 
+            title="Restaurantes" 
+            subtitle="Descubre nuestras opciones" 
+            backgroundImage="/images/restaurantes.jpg" 
+          />
+        </div>
+
+        <main className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
+          {/* Columna izquierda */}
+          <div className="lg:col-span-2 flex flex-col gap-6">
+            <Informacion restaurante={restauranteSeleccionado} />
+            <Carousel restaurante={restauranteSeleccionado} />
+            {/* <Descripcion restaurante={restauranteSeleccionado} /> */}
+            <Caracteristicas restaurante={restauranteSeleccionado} />
+          </div>
+          
+          {/* Columna derecha */}
+          <div className="lg:col-span-1 flex flex-col h-full gap-6">
+            {/* Contenedor para Contacto y Mapa */}
+            <div className="flex flex-col flex-1 gap-6">
+              {/* Contacto */}
+              <div className="flex-1">
+                <Contacto restaurante={restauranteSeleccionado} />
+              </div>
+              {/* Mapa */}
+              <div className="flex-1">
+                <UbicacionMap ubicacion={restauranteSeleccionado.coordenadas} />
+              </div>
+            </div>
+            {/* Publicaciones Similares */}
+            <div className="mt-auto">
+              <PublicacionesSimilares restaurantes={restaurantesSimilares} />
+            </div>
+          </div>
+        </main>
       </div>
-      {memoizedContent}
-    </div>
+    </Suspense>
   );
 };
 
