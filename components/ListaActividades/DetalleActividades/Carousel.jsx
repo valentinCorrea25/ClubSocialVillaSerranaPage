32eae26b04559ec96eeadc5e1fd061b1de21c94d
tomascontilde <tomazcontilde@gmail.com>
2024-10-14 23:27:46 -0300

    detalle de actividad, maniana detalle de eventos/noticias si hay algun error avisa pe

---
+++ components/ListaActividades/DetalleActividades/Carousel.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Carousel as AntdCarousel } from 'antd';
+
+const Carousel = ({ actividad }) => {
+  
+  const fotos = actividad.fotos;
+  console.log( fotos + 'fotos');
+  if(!fotos){
+    return null
+  }
+
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md">
+      <AntdCarousel arrows infinite={false}>
+        {fotos.map((foto, index) => (
+          <div key={index} className="flex justify-center items-center h-[500px] w-full">
+            <img src={foto} alt={`${actividad.Titulo} - ${index + 1}`} className="w-full h-full object-cover" />
+          </div>
+        ))}
+      </AntdCarousel>
+    </div>
+  );
+};
+
+export default Carousel;
