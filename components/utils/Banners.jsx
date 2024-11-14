35b3b93f0d526ee024372de799909c9bb969a5fd
valentinCorrea25 <correavalentin89@gmail.com>
2024-11-13 21:13:43 -0300

    fix serivicos

--- components/utils/Banners.jsx
+++ components/utils/Banners.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = ({ title, subtitle, backgroundImage }) => (
   <div
-    className="bg-cover bg-center h-64 rounded-lg mx-3 overflow-hidden relative max-w-screen-xl"
+    className="bg-cover bg-center h-64 rounded-lg overflow-hidden relative max-w-screen-xl mx-auto"
     style={{ backgroundImage: `url(${backgroundImage})`, filter: 'brightness(70%)', padding: "40px 10px" }}
   >
     <div className="absolute inset-0 flex items-center justify-center bg-green-800 bg-opacity-50 rounded-lg">
