86e5749f3edfeede6800b467eb3708a7556217a2
tomascontilde <tomazcontilde@gmail.com>
2024-11-19 12:50:00 -0300

    cambios en el banner de sobre nosotros pe

--- app/sobrenosotros/page.js
+++ app/sobrenosotros/page.js
@@ -3,15 +3,19 @@ import { SobreElCLub, Sobrevillaserrana } from '@/components/SobreNosotros/Sobre
 import Banner from "@/components/utils/Banners"
 export default function SobreNosotros() {
   return (
-    <div className='min-h-screen bg-[#F9F6EE] sm:px-12 lg:px-20 py-5'>
-      <Banner
-        title="Historia"
-        subtitle="Descubre un poco sobre nosotros"
-        backgroundImage="/images/sobrenosotros.jpeg"
 
-      />
-      <SobreElCLub />
-      <Sobrevillaserrana />
-    </div>
+    <>
+      <div className="p-[10px] mx-auto max-w-[1200px]">
+        <Banner
+          title="Historia"
+          subtitle="Descubre un poco sobre nosotros"
+          backgroundImage="/images/sobrenosotros.jpeg"
+        />
+      </div>
+      <div className='min-h-screen bg-[#F9F6EE] sm:px-12 lg:px-20 py-5'>
+        <SobreElCLub />
+        <Sobrevillaserrana />
+      </div>
+    </>
   )
 }
