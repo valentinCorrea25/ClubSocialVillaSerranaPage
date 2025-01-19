d2f2550e2c4313c28f737fdae0352f81e8228c70
valentinCorrea25 <correavalentin89@gmail.com>
2025-01-19 17:38:31 -0300

    fix

--- app/api/usuarios/lista/route.js
+++ app/api/usuarios/lista/route.js
@@ -1,6 +1,6 @@
 import prisma from "@/libs/prisma";
 import { NextResponse } from "next/server";
-
+export const dynamic = 'force-dynamic';
 export async function GET() {
   try {
     const usuarios = await prisma.usuario.findMany({
