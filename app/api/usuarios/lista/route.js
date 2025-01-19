61237f6c76a98d96ba7b0d908e3a6be9d675c75c
valentinCorrea25 <correavalentin89@gmail.com>
2025-01-19 17:20:52 -0300

    fix

--- app/api/usuarios/lista/route.js
+++ app/api/usuarios/lista/route.js
@@ -5,19 +5,25 @@ export async function GET() {
   try {
     const usuarios = await prisma.usuario.findMany({
       select: {
-        nombre:true,
+        nombre: true,
         id: true
       }
     });
+    const response = NextResponse.json({
+      usuarios,
+      date: new Date().toString()
+    });
 
-    return NextResponse.json({
-        usuarios,
-        date: new Date().toString()
-      });
+    // Añadir headers anti-caché
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate');
+    response.headers.set('Pragma', 'no-cache');
+    response.headers.set('Expires', '0');
+
+    return response;
+
   } catch (error) {
     return NextResponse.json({
-      message:
-        "Hubo un error en Usuarios, contactar con desarollador",
+      message: "Hubo un error en Usuarios, contactar con desarollador",
       code: 500,
     });
   }
